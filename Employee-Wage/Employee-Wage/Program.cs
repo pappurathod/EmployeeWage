7f43648632123c081258cfa7ccad106fc19eeeb1
pappurathod <prathod4545@gmail.com>
2022-03-06 10:06:49 +0530

    Check Employee Attendance

--- Employee-Wage/Employee-Wage/Program.cs
+++ Employee-Wage/Employee-Wage/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage Computation program");
+//UC1Attendance
+int Present = 1;
+Random random = new Random();
+int EmployeeStatus = random.Next(0, 2);
+if (EmployeeStatus == Present)
+{
+    Console.WriteLine("Employee is present");
+}
+else
+{
+    Console.WriteLine("Employee is absent");
+}
