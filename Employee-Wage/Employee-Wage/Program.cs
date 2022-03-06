981d9bdf98f7f02d0d51caa5804d6bd508708d94
pappurathod <prathod4545@gmail.com>
2022-03-06 10:14:33 +0530

    Calculate Employee Daily Wage

--- Employee-Wage/Employee-Wage/Program.cs
+++ Employee-Wage/Employee-Wage/Program.cs
@@ -1,14 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage Computation program");
-//UC1Attendance
+//UC2-Calculate_Daily_Wage
 int Present = 1;
+int Emp_per_rate = 20;
+int emphr = 0;
+int empwage = 0;
+
 Random random = new Random();
-int EmployeeStatus = random.Next(0, 2);
-if (EmployeeStatus == Present)
+int Employeestatus = random.Next(0, 2);
+if (Employeestatus == Present)
 {
     Console.WriteLine("Employee is present");
+    emphr = 8;
 }
 else
 {
     Console.WriteLine("Employee is absent");
+    emphr = 0;
 }
+empwage = (Emp_per_rate * emphr);
+Console.WriteLine("Daily Wage " + empwage);
