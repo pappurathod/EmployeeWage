e9a9d3efc8290ce174b79f6ac25be1bb8a39dd1b
pappurathod <prathod4545@gmail.com>
2022-03-07 12:19:47 +0530

    Calculate Emp daily wage for a month

--- Employee-Wage/Employee-Wage/Program.cs
+++ Employee-Wage/Employee-Wage/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage Computation program");
 
-//UC4-Employee_Wage_Using_Case_Statement
+//UC5-Calculate_Employee_Wage_For_Month
 
 namespace EmployeeWage
 {
@@ -11,39 +11,39 @@ namespace EmployeeWage
         public const int IsFullTime = 1;
         public const int IsPartTime = 2;
         public const int Emp_per_rate = 20;
+        public const int Num_Of_Working_Days = 20;
 
-       public static void Main(String[] args)
+        static void Main(String[] args)
         {
             int emphr = 0;
             int empwage = 0;
+            int totalEmpWage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-            switch (empCheck)
+            for (int day = 0; day < Num_Of_Working_Days; day++)
             {
-                case IsFullTime:
-                    Console.WriteLine("Employee is FullTime present");
-                    emphr = 8;
-                    //empwage = (Emp_per_rate * emphr);
-                    //Console.WriteLine("Daily wage " + empwage);
-                    break;
-                case IsPartTime:
-                    Console.WriteLine("Employee is Part Time present");
-                    emphr = 4;
-                    //empwage = (Emp_per_rate * emphr);
-                    //Console.WriteLine("Daily wage " + empwage);
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent");
-                    emphr = 0;
-                    //empwage = (Emp_per_rate * emphr);
-                    //Console.WriteLine("Daily wage " + empwage);
-                    break;
-            }
-            empwage = (Emp_per_rate * emphr);
-            Console.WriteLine("Daily wage " + empwage);
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
 
+                switch (empCheck)
+                {
+                    case IsFullTime:
+                        Console.WriteLine("Employee is FullTime present");
+                        emphr = 8;
+                        break;
+                    case IsPartTime:
+                        Console.WriteLine("Employee is Part Time present");
+                        emphr = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        emphr = 0;
+                        break;
+                }
+                empwage = (Emp_per_rate * emphr);
+                totalEmpWage += empwage;
+                Console.WriteLine("Daily Emp wage : " + empwage);
+            }
+            Console.WriteLine("total Emp wage : " + totalEmpWage);
         }
     }
 }
