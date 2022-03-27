f8260c53bf1c59dacef3e87e7f025a5cb05102ae
pappurathod <prathod4545@gmail.com>
2022-03-27 18:11:24 +0530

    Using Interface

---
+++ Employee-Wage/Employee-Wage/CalEmpWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage
+{
+    internal interface CalEmpWage
+    {
+        void AddCompany(String CompanyName, int EmpWagePerHour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHrs_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS);
+        int CalculateEmpWage(string CompanyName);
+    }
+}
